37c95e642b08e289101c3e48ddef5d1a756cad8d
vadimKapitan <xxxcap13@gmail.com>
2023-09-06 21:59:26 +0300

    ДЗ готово. Просьба проверить.

--- CalculatorApplication.java
+++ CalculatorApplication.java
@@ -8,6 +8,5 @@ public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
-		
 	}
 }
---
+++ ServiceCalculator.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator;
+
+public interface ServiceCalculator {
+    String welcome(String welcome);
+
+    int plusValue(int num1, int num2);
+
+    int minusValue(int num1, int num2);
+
+    int multyplyValue(int num1, int num2);
+
+    int divideValue(int num1, int num2);
+}
